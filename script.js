bafb062b95813e6d0d4f8ecc762a352c98426396
Mmamotsatsi <voucalproductions@gmail.com>
2026-08-20 17:14:27 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+const toggle = document.getElementById("themeToggle");
 
+if (localStorage.getItem("theme") === "dark") {
+  document.body.classList.add("dark");
+  toggle.textContent = "☀";
+}
+
+toggle.addEventListener("click", () => {
+  document.body.classList.toggle("dark");
+
+  const darkMode = document.body.classList.contains("dark");
+
+  localStorage.setItem("theme", darkMode ? "dark" : "light");
+  toggle.textContent = darkMode ? "☀" : "☾";
+});
